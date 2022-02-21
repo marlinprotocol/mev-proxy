f35f0fb1d675537c545eba37daa909de5b003db6
Supragya Raj <supragyaraj@gmail.com>
2022-02-21 13:17:38 +0530

    token bucket rate limiter

--- main.go
+++ main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
+
+	"golang.org/x/time/rate"
 )
 
 func main() {
 	listenAddrPtr := flag.String("listenAddr", "127.0.0.1:18545", "listen address")
 	rpcAddrPtr := flag.String("rpcAddr", "127.0.0.1:8545", "rpc address")
 	subgraphPathPtr := flag.String("subgraphPath", "/marlinprotocol/mev-bor", "subgraph path")
+	limiterTimePtr := flag.Int64("limiterTime", 5, "allow one bundle every <X> ms")
+	limiterBurstPtr := flag.Int("limiterBurst", 3, "max burst for rate limiter")
 
 	flag.Parse()
 
 	fmt.Printf("Starting gateway with listenAddr: %s, rpcAddr: %s\n", *listenAddrPtr, *rpcAddrPtr)
 
-	g := &Proxy{*rpcAddrPtr, nil, *subgraphPathPtr}
+	limit := rate.Every(time.Millisecond * time.Duration(*limiterTimePtr))
+	limiter := rate.NewLimiter(limit, *limiterBurstPtr)
+	g := &Proxy{*rpcAddrPtr, nil, *subgraphPathPtr, limiter}
 	g.ListenAndServe(*listenAddrPtr)
 }
